16bfb4d1e101a0ca9ee0aff152d28a6d81b70588
jspark <sylvan0212@gmail.com>
2021-10-20 15:45:59 +0900

    finished lession 17

---
+++ infra/stack/vpc_stack.ts
@@ -0,0 +1,46 @@
+import { aws_ec2, Stack, StackProps, aws_ssm } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class VPCStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const prj_name = this.node.tryGetContext("project_name");
+    const env_name = this.node.tryGetContext("env");
+
+    const vpc = new aws_ec2.Vpc(this, "devVPC", {
+      cidr: "172.32.0.0/16",
+      maxAzs: 2,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      subnetConfiguration: [
+        {
+          name: "Public",
+          subnetType: aws_ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: "Private",
+          subnetType: aws_ec2.SubnetType.PRIVATE_WITH_NAT,
+          cidrMask: 24,
+        },
+        {
+          name: "Isolated",
+          subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+      natGateways: 1,
+    });
+
+    vpc.privateSubnets
+      .map((subnet) => subnet.subnetId)
+      .forEach((id, index) => {
+        const name = `private-subnet-${index + 1}`;
+        new aws_ssm.StringParameter(this, name, {
+          stringValue: id,
+          parameterName: `/${env_name}/${name}`,
+        });
+      });
+  }
+}
--- test/devops.test.ts
+++ test/devops.test.ts
@@ -1,11 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import * as Devops from '../lib/devops-stack';
+import * as cdk from "aws-cdk-lib";
+import * as Devops from "../infra/stack/devops-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Devops.DevopsStack(app, 'MyTestStack');
-    // THEN
-    const actual = app.synth().getStackArtifact(stack.artifactId).template;
-    expect(actual.Resources ?? {}).toEqual({});
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Devops.DevopsStack(app, "MyTestStack");
+  // THEN
+  const actual = app.synth().getStackArtifact(stack.artifactId).template;
+  expect(actual.Resources ?? {}).toEqual({});
 });
